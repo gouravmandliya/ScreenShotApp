77f94dab12515f92d864ee06d8f6ba73dcc3f410
Gourav Mandliya <gourav.mandliya@xplortechnologies.com>
2024-06-06 16:46:20 +0530

    initial commit

---
+++ ScreenShotApp/ScreenShotApp/ScreenShotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotAppApp.swift
+//  ScreenShotApp
+//
+//  Created by GOURAVM on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenShotApp/ScreenShotApp/ViewModel/ScreenCaptureViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenCaptureViewModel.swift
+//  ScreenShotApp
+//
+//  Created by GOURAVM on 06/06/24.
+//
+
+import SwiftUI
+
+struct ScreenCaptureViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScreenCaptureViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ScreenCaptureViewModel()
+    }
+}
