8ae57074516c5040f14be012dc8f856b149753e1
Gourav Mandliya <gourav.mandliya@xplortechnologies.com>
2024-06-07 13:58:41 +0530

    menu bar added

---
+++ ScreenShotApp/ScreenShotApp/Views/MenuBar.swift
@@ -0,0 +1,20 @@
+//
+//  MenuBar.swift
+//  ScreenShotApp
+//
+//  Created by GOURAVM on 07/06/24.
+//
+
+import SwiftUI
+
+struct MenuBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuBar_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuBar()
+    }
+}
